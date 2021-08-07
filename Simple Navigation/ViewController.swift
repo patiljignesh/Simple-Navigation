e9bb7e1db9b9295563686a99c2247f9a32c1fce8
Jignesh Patil <patiljignesh@gmail.com>
2021-08-07 18:17:14 +0200

    Initial Commit

---
+++ Simple Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Navigation
+//
+//  Created by Jigneshkumar Patil on 2021/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
